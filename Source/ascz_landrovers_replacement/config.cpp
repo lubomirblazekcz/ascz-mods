b45ac8a03a3a213abc137463d78ed32e180c2cfa
EvroMalarkey <evromalarkey@gmail.com>
2014-11-11 14:39:44 +0100

    landrover replacement for acr_a3 and kae_sz

---
+++ Source/ascz_landrovers_replacement/config.cpp
@@ -0,0 +1,71 @@
+class CfgPatches
+{
+	class ascz_landrovers_replacement
+	{
+		units[] = {"LandRover_CZ_EP1", "LandRover_ACR", "LandRover_MG_TK_EP1", "ACR_Offroad_HMG", "ACR_Offroad_HMG_Desert", "ACR_LandRover_AMB"};
+		weapons[] = {};
+		magazines[] = {};
+		ammo[] = {};
+		requiredAddons[] = {"PansyLandRovers"};
+	};
+};
+class CfgVehicles {
+    class BAF_Offroad_D;
+    class LandRover_MG_Base_EP1;
+    class BAF_Offroad_W_HMG;
+    class LandRover_AMB_Base;
+	class LandRover_CZ_EP1: BAF_Offroad_D
+	{
+		displayname = "Land Rover (Desert)";
+		faction = "ACR";
+        crew = "ACR_MOUT_Vojak01";
+        typicalCargo[] = {"ACR_MOUT_Vojak01"};
+		hiddenSelectionsTextures[] = {"pansy\LandRover\data\lr_base_acrs_co","pansy\LandRover\data\lr_special_acr_co"};
+	};
+	class LandRover_ACR: LandRover_CZ_EP1
+	{
+		displayname = "Land Rover";
+		faction = "ACR";
+        crew = "ACR_MOUT_Vojak01";
+        typicalCargo[] = {"ACR_MOUT_Vojak01"};
+		hiddenSelectionsTextures[] = {"pansy\LandRover\data\lr_acr_base_co","pansy\LandRover\data\lr_acr_spec_co"};
+	};
+	class ACR_Offroad_HMG: BAF_Offroad_W_HMG
+	{
+		scope = 2;
+		displayname = "Land Rover HMG";
+		faction = "ACR";
+        crew = "ACR_MOUT_Vojak01";
+        typicalCargo[] = {"ACR_MOUT_Vojak01"};
+		hiddenSelectionsTextures[] = {"pansy\LandRover\data\lr_acr_base_co","pansy\LandRover\data\lr_acr_spec_co","pansy\LandRover\data\lr_mg-spg9_ind_co"};
+	};
+	class ACR_Offroad_HMG_Desert: BAF_Offroad_W_HMG
+	{
+		scope = 2;
+		displayname = "Land Rover HMG (Desert)";
+		faction = "ACR";
+        crew = "ACR_MOUT_Vojak01";
+        typicalCargo[] = {"ACR_MOUT_Vojak01"};
+		hiddenSelectionsTextures[] = {"pansy\LandRover\data\lr_base_acrs_co","pansy\LandRover\data\lr_acr_spec_co","pansy\LandRover\data\lr_mg-spg9_ind_co"};
+	};
+	class ACR_LandRover_AMB: LandRover_AMB_Base
+	{
+		displayName = "Land Rover Ambulance";
+		scope = 2;
+		faction = "ACR";
+        crew = "ACR_MOUT_Vojak01";
+        typicalCargo[] = {"ACR_MOUT_Vojak01"};
+	};
+	class LandRover_MG_TK_EP1: LandRover_MG_Base_EP1
+	{
+		scope = 2;
+		displayname = "Land Rover HMG";
+		side = 0;
+		hiddenSelectionsTextures[] = {"pansy\LandRover\data\lr_base_ind_co","pansy\LandRover\data\lr_special_w_baf_co","pansy\LandRover\data\lr_mg-spg9_ind_co"};
+		typicalCargo[] = {"kae_TK_rifleman"};
+        author = "Kaelies";
+        vehicleClass = "Car";
+        crew = "kae_TK_rifleman";
+		faction = "Kae_TK";
+	};
+};
