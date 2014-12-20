224504cd25d764edaf589dcc975300a6f3ccde5a
EvroMalarkey <evromalarkey@gmail.com>
2014-12-20 12:26:00 +0100

    no voice in arsenal

---
+++ Source/ascz_voice_fix/config.cpp
@@ -0,0 +1,180 @@
+class CfgPatches
+{
+	class ascz_voice_fix
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = {"RHS_RU_Radio"};
+		version = "1.0";
+		author[] = {"EvroMalarkey"};
+	};
+};
+class CfgVoice
+{
+	class ENG;
+	class Male01ENG: ENG
+	{
+		scope = 1;
+	};
+	class Male02ENG: ENG
+	{
+		scope = 1;
+	};
+	class Male03ENG: ENG
+	{
+		scope = 1;
+	};
+	class Male04ENG: ENG
+	{
+		scope = 1;
+	};
+	class Male05ENG: ENG
+	{
+		scope = 1;
+	};
+	class Male06ENG: ENG
+	{
+		scope = 1;
+	};
+	class Male07ENG: ENG
+	{
+		scope = 1;
+	};
+	class Male08ENG: ENG
+	{
+		scope = 1;
+	};
+	class Male09ENG: ENG
+	{
+		scope = 1;
+	};
+	class Male10ENG: ENG
+	{
+		scope = 1;
+	};
+	class Male11ENG: ENG
+	{
+		scope = 1;
+	};
+	class Male12ENG: ENG
+	{
+		scope = 1;
+	};
+	class ENGB;
+	class Male01ENGB: ENGB
+	{
+		scope = 1;
+	};
+	class Male02ENGB: ENGB
+	{
+		scope = 1;
+	};
+	class Male03ENGB: ENGB
+	{
+		scope = 1;
+	};
+	class Male04ENGB: ENGB
+	{
+		scope = 1;
+	};
+	class Male05ENGB: ENGB
+	{
+		scope = 1;
+	};
+	class GRE;
+	class Male01GRE: GRE
+	{
+		scope = 1;
+	};
+	class Male02GRE: GRE
+	{
+		scope = 1;
+	};
+	class Male03GRE: GRE
+	{
+		scope = 1;
+	};
+	class Male04GRE: GRE
+	{
+		scope = 1;
+	};
+	class Male05GRE: GRE
+	{
+		scope = 1;
+	};
+	class Male06GRE: GRE
+	{
+		scope = 1;
+	};
+	class PER;
+	class Male01PER: PER
+	{
+		scope = 1;
+	};
+	class Male02PER: PER
+	{
+		scope = 1;
+	};
+	class Male03PER: PER
+	{
+		scope = 1;
+	};
+	class ENGVR;
+	class Male01ENGVR: ENGVR
+	{
+		scope = 1;
+	};
+	class GREVR;
+	class Male01GREVR: GREVR
+	{
+		scope = 1;
+	};
+	class PERVR;
+	class Male01PERVR: PERVR
+	{
+		scope = 1;
+	};
+	class CZ_ACR;
+	class Male01CZ_ACR: CZ_ACR
+	{
+		scope = 1;
+	};
+	class Male02CZ_ACR: CZ_ACR
+	{
+		scope = 1;
+	};
+	class Male03CZ_ACR: CZ_ACR
+	{
+		scope = 1;
+	};
+	class Male04CZ_ACR: CZ_ACR
+	{
+		scope = 1;
+	};
+	class Male05CZ_ACR: CZ_ACR
+	{
+		scope = 1;
+	};
+	class RUS;
+	class rhs_Male01RUS: RUS
+	{
+		scope = 1;
+	};
+	class rhs_Male02RUS: RUS
+	{
+		scope = 1;
+	};
+	class rhs_Male03RUS: RUS
+	{
+		scope = 1;
+	};
+	class rhs_Male04RUS: RUS
+	{
+		scope = 1;
+	};
+	class rhs_Male05RUS: RUS
+	{
+		scope = 1;
+	};
+};
