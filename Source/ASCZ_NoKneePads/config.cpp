d3d4263ba811e6911f046526ba9674682cefe8fa
taurus <xxxTaurus@gmail.com>
2014-12-21 15:16:41 +0100

    ASCZ_NoKneePads - Added Tan Variants
    also Binarized.

--- Source/ASCZ_NoKneePads/config.cpp
+++ Source/ASCZ_NoKneePads/config.cpp
@@ -9,7 +9,7 @@ class CfgPatches
 	};
 };
 class cfgWeapons {
-    // --- UNIFORMS --- //
+// --- UNIFORMS --- //Olive
     class Uniform_Base;
 	class UniformItem;
  	class U_B_CombatUniform_NKPOlive: Uniform_Base {
@@ -76,9 +76,73 @@ class cfgWeapons {
             mass = 40;
         };
 	};
+// --- UNIFORMS --- //Tan
+ 	class U_B_CombatUniform_NKPTan: Uniform_Base {
+ 	    scope = 2;
+ 	    displayName = "$STR_NKP_Tan";
+		picture = "\ASCZ_NoKneePads\data\UI\icon_tan_uniform.paa";
+		author = "Taurus";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"\ASCZ_NoKneePads\data\Suitpacks\suitpack_NKPTan.paa"};
+        class ItemInfo: UniformItem
+        {
+            uniformModel = "-";
+            uniformClass = "B_SoldierTan_F";
+            containerClass = "Supply40";
+            mass = 40;
+        };
+    };
+ 	class U_B_CombatUniform_NKPTan_tshirt: Uniform_Base {
+ 	    scope = 2;
+ 	    displayName = "$STR_NKP_tan_tee";
+		picture = "\ASCZ_NoKneePads\data\UI\icon_tan_tshirt.paa";
+		author = "Taurus";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"\ASCZ_NoKneePads\data\Suitpacks\suitpack_NKPTan.paa"};
+ 	    class ItemInfo: UniformItem
+        {
+            uniformModel = "-";
+            uniformClass = "B_SoldierTan_02_F";
+            containerClass = "Supply40";
+            mass = 20;
+        };
+	};
+ 	class U_B_CombatUniform_NKPTan_vest: Uniform_Base {
+ 	    scope = 2;
+ 	    displayName = "$STR_Recon_NKP_tan";
+		picture = "\ASCZ_NoKneePads\data\UI\icon_tan_vest.paa";
+		author = "Taurus";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"\ASCZ_NoKneePads\data\Suitpacks\suitpack_NKPTan.paa"};
+ 	    class ItemInfo: UniformItem
+        {
+            uniformModel = "-";
+            uniformClass = "B_SoldierTan_03_F";
+            containerClass = "Supply40";
+            mass = 40;
+        };
+	};
+ 	class U_B_CombatUniform_NKPTan_rollup: Uniform_Base {
+ 	    scope = 2;
+ 	    displayName = "$STR_NKP_Tan_rollup";
+		picture = "\ASCZ_NoKneePads\data\UI\icon_tan_rollup.paa";
+		author = "Taurus";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"\ASCZ_NoKneePads\data\Suitpacks\suitpack_NKPTan.paa"};
+ 	    class ItemInfo: UniformItem
+        {
+            uniformModel = "-";
+            uniformClass = "B_SoldierTan_04_F";
+            containerClass = "Supply40";
+            mass = 40;
+        };
+	};
 };
-// --- UNIFORM BASE --- //
-
+// --- UNIFORM BASE --- //Olive
 class UniformSlotInfo;
 class cfgVehicles {
     class B_Soldier_base_F;
@@ -159,6 +223,85 @@ class cfgVehicles {
         hiddenSelections[] = {"camo1","camo2","insignia"};
 		hiddenSelectionsMaterials[] = {"","\ASCZ_NoKneePads\data\NKP.rvmat"};
         hiddenSelectionsTextures[] = {"\ASCZ_NoKneePads\data\NKP_shirt.paa","\ASCZ_NoKneePads\data\NKP_olive.paa"};
+        weapons[] = {"arifle_MX_Black_Hamr_pointer_F","hgun_P07_F","Throw","Put"};
+        respawnWeapons[] = {"arifle_MX_Black_Hamr_pointer_F","hgun_P07_F","Throw","Put"};
+    };
+// --- UNIFORM BASE --- //Tan
+	class B_SoldierTan_F: B_Soldier_base_F
+    {
+        author = "$STR_A3_Bohemia_Interactive";
+        _generalMacro = "B_Soldier_F";
+        scope = 2;
+        displayName = "Rifleman 1 Tan";
+        uniformAccessories[] = {};
+        nakedUniform = "U_BasicBody";
+        uniformClass = "U_B_CombatUniform_NKPTan";
+        hiddenSelections[] = {"Camo","insignia"};
+		hiddenSelectionsMaterials[] = {"\ASCZ_NoKneePads\data\NKP.rvmat"};
+        hiddenSelectionsTextures[] = {"\ASCZ_NoKneePads\data\NKP_Tan.paa"};
+        weapons[] = {"arifle_MX_Black_Hamr_pointer_F","hgun_P07_F","Throw","Put"};
+        respawnWeapons[] = {"arifle_MX_Black_Hamr_pointer_F","hgun_P07_F","Throw","Put"};
+        class UniformInfo
+        {
+            class SlotsInfo
+            {
+                class NVG: UniformSlotInfo
+                {
+                    slotType = 602;
+                };
+                class Scuba: UniformSlotInfo
+                {
+                    slotType = 604;
+                };
+                class Headgear: UniformSlotInfo
+                {
+                    slotType = 605;
+                };
+            };
+        };
+    };
+    class B_SoldierTan_02_f: B_Soldier_02_f
+    {
+        author = "$STR_A3_Bohemia_Interactive";
+        _generalMacro = "B_Soldier_02_f";
+        scope = 2;
+        displayName = "Rifleman 2 Tan";
+        model = "\A3\characters_F\BLUFOR\b_soldier_02.p3d";
+        modelSides[] = {3,1};
+        uniformClass = "U_B_CombatUniform_NKPTan_tshirt";
+        hiddenSelections[] = {"Camo", "camo2", "insignia"};
+		hiddenSelectionsMaterials[] = {"\ASCZ_NoKneePads\data\NKP.rvmat"};
+        hiddenSelectionsTextures[] = {"\ASCZ_NoKneePads\data\NKP_Tan.paa", "\a3\characters_f\common\data\basicbody_black_co.paa"};
+        weapons[] = {"arifle_MX_Black_Hamr_pointer_F","hgun_P07_F","Throw","Put"};
+        respawnWeapons[] = {"arifle_MX_Black_Hamr_pointer_F","hgun_P07_F","Throw","Put"};
+    };
+    class B_SoldierTan_03_f: B_Soldier_03_f
+    {
+        author = "$STR_A3_Bohemia_Interactive";
+        _generalMacro = "B_Soldier_03_f";
+        scope = 2;
+        displayName = "Rifleman 3 Tan";
+        model = "\A3\characters_F\BLUFOR\b_soldier_03.p3d";
+        modelSides[] = {3,1};
+        uniformClass = "U_B_CombatUniform_NKPTan_vest";
+        hiddenSelections[] = {"Camo","insignia"};
+		hiddenSelectionsMaterials[] = {"\ASCZ_NoKneePads\data\NKP.rvmat"};
+        hiddenSelectionsTextures[] = {"\ASCZ_NoKneePads\data\NKP_Tan.paa"};
+        weapons[] = {"arifle_MX_Black_Hamr_pointer_F","hgun_P07_F","Throw","Put"};
+        respawnWeapons[] = {"arifle_MX_Black_Hamr_pointer_F","hgun_P07_F","Throw","Put"};
+    };
+    class B_SoldierTan_04_f: B_Soldier_03_f
+    {
+        author = "$STR_A3_Bohemia_Interactive";
+        _generalMacro = "B_Soldier_03_f";
+        scope = 2;
+        displayName = "Rifleman 4 Tan";
+        model = "\A3\Characters_F_EPB\Guerrilla\ig_guerrilla5_1.p3d";
+        modelSides[] = {3,1};
+        uniformClass = "U_B_CombatUniform_NKPTan_rollup";
+        hiddenSelections[] = {"camo1","camo2","insignia"};
+		hiddenSelectionsMaterials[] = {"","\ASCZ_NoKneePads\data\NKP.rvmat"};
+        hiddenSelectionsTextures[] = {"\ASCZ_NoKneePads\data\NKP_shirt.paa","\ASCZ_NoKneePads\data\NKP_Tan.paa"};
         weapons[] = {"arifle_MX_Black_Hamr_pointer_F","hgun_P07_F","Throw","Put"};
         respawnWeapons[] = {"arifle_MX_Black_Hamr_pointer_F","hgun_P07_F","Throw","Put"};
     };
