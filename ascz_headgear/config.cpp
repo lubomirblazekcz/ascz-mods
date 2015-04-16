cd1aa9b6f0355be0b3478c05261e1b1bb4418ec6
taurus <xxxTaurus@gmail.com>
2015-04-16 17:35:41 +0200

    Only UI missing for Cowboy hat

--- ascz_headgear/config.cpp
+++ ascz_headgear/config.cpp
@@ -10,6 +10,7 @@ class CfgPatches {
 class cfgWeapons
 {
 	class H_Cap_oli_hs;
+	class H_Cap_headphones;
 	class ItemInfo;
 	class H_Bandana_khaki: H_Cap_oli_hs //Bandana
 	{
@@ -60,7 +61,7 @@ class cfgWeapons
 	    hiddenSelectionsTextures[] = {"\ascz_headgear\data\Bandana_olive_co.paa"};
 	    class ItemInfo: ItemInfo{};
 	};
-	class H_CowboyHat_brown: H_Bandana_olive //CowboyHat
+	class H_Cowboy_Hat_brown: H_Cap_headphones //CowboyHat
 	{
 		author = "ASCZ";
 		displayName = "$STR_ASCZ_CowboyHat_Brown";
@@ -71,6 +72,33 @@ class cfgWeapons
 		hiddenSelectionsTextures[] = {"\ascz_headgear\data\CowboyHat_brown_co.paa"};
         class ItemInfo: ItemInfo{
 			uniformModel = "\ascz_headgear\CowboyHat.p3d";
-		};
+	    };
 	};
-};
+	class H_Cowboy_Hat_black: H_Cowboy_Hat_brown
+	{
+	author = "ASCZ";
+	displayName = "$STR_ASCZ_CowboyHat_Black";
+	picture = "\ascz_headgear\data\UI\icon_Bandana_olive.paa";
+	
+	    hiddenSelectionsTextures[] = {"\ascz_headgear\data\CowboyHat_black_co.paa"};
+	    class ItemInfo: ItemInfo{};
+	};
+	class H_Cowboy_Hat_green: H_Cowboy_Hat_black
+	{
+	author = "ASCZ";
+	displayName = "$STR_ASCZ_CowboyHat_Green";
+	picture = "\ascz_headgear\data\UI\icon_Bandana_olive.paa";
+	
+	    hiddenSelectionsTextures[] = {"\ascz_headgear\data\CowboyHat_green_co.paa"};
+	    class ItemInfo: ItemInfo{};
+	};
+	class H_Cowboy_Hat_white: H_Cowboy_Hat_green
+	{
+	author = "ASCZ";
+	displayName = "$STR_ASCZ_CowboyHat_White";
+	picture = "\ascz_headgear\data\UI\icon_Bandana_olive.paa";
+	
+	    hiddenSelectionsTextures[] = {"\ascz_headgear\data\CowboyHat_white_co.paa"};
+	    class ItemInfo: ItemInfo{};
+        };
+    };
