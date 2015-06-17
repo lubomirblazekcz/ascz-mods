beff8662d32a5e687640d944ece4981b5817e611
taurus <xxxTaurus@gmail.com>
2015-06-17 19:26:11 +0200

    EVRO BINARIZE THAT SHIT.

--- ascz_headgear/config.cpp
+++ ascz_headgear/config.cpp
@@ -18,7 +18,6 @@ class cfgWeapons
 		displayName = "$STR_ASCZ_Bandana_Khaki";
 		picture = "\ascz_headgear\data\UI\icon_Bandana_khaki.paa";
 
-		passThrough = 1;
 		model = "\ascz_headgear\Bandana.p3d";
 		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\ascz_headgear\data\Bandana_khaki_co.paa"};
@@ -32,8 +31,7 @@ class cfgWeapons
 	displayName = "$STR_ASCZ_Bandana_Black";
 	picture = "\ascz_headgear\data\UI\icon_Bandana_black.paa";
 	
-	    passThrough = 1;
-		hiddenSelectionsTextures[] = {"\ascz_headgear\data\Bandana_black_co.paa"};
+	    hiddenSelectionsTextures[] = {"\ascz_headgear\data\Bandana_black_co.paa"};
 	    class ItemInfo: ItemInfo{};
 	};
 	class H_Bandana_brown: H_Bandana_black
@@ -42,8 +40,7 @@ class cfgWeapons
 	displayName = "$STR_ASCZ_Bandana_Brown";
 	picture = "\ascz_headgear\data\UI\icon_Bandana_brown.paa";
 	
-	    passThrough = 1;
-		hiddenSelectionsTextures[] = {"\ascz_headgear\data\Bandana_brown_co.paa"};
+	    hiddenSelectionsTextures[] = {"\ascz_headgear\data\Bandana_brown_co.paa"};
 	    class ItemInfo: ItemInfo{};
 	};
 	class H_Bandana_red: H_Bandana_brown
@@ -52,8 +49,7 @@ class cfgWeapons
 	displayName = "$STR_ASCZ_Bandana_Red";
 	picture = "\ascz_headgear\data\UI\icon_Bandana_red.paa";
 	
-	    passThrough = 1;
-		hiddenSelectionsTextures[] = {"\ascz_headgear\data\Bandana_red_co.paa"};
+	    hiddenSelectionsTextures[] = {"\ascz_headgear\data\Bandana_red_co.paa"};
 	    class ItemInfo: ItemInfo{};
 	};
 	class H_Bandana_olive: H_Bandana_red
@@ -62,8 +58,7 @@ class cfgWeapons
 	displayName = "$STR_ASCZ_Bandana_Olive";
 	picture = "\ascz_headgear\data\UI\icon_Bandana_olive.paa";
 	
-	    passThrough = 1;
-		hiddenSelectionsTextures[] = {"\ascz_headgear\data\Bandana_olive_co.paa"};
+	    hiddenSelectionsTextures[] = {"\ascz_headgear\data\Bandana_olive_co.paa"};
 	    class ItemInfo: ItemInfo{};
 	};
 	class H_Cowboy_Hat_brown: H_Cap_headphones //CowboyHat
@@ -72,8 +67,6 @@ class cfgWeapons
 		displayName = "$STR_ASCZ_CowboyHat_Brown";
 		picture = "\ascz_headgear\data\UI\icon_CowboyHat_brown.paa";
 
-		mass = 6;
-		passThrough = 1;
 		model = "\ascz_headgear\CowboyHat.p3d";
 		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {"\ascz_headgear\data\CowboyHat_brown_co.paa"};
@@ -81,37 +74,3 @@ class cfgWeapons
 			uniformModel = "\ascz_headgear\CowboyHat.p3d";
 	    };
 	};
-	class H_Cowboy_Hat_black: H_Cowboy_Hat_brown
-	{
-	author = "ASCZ";
-	displayName = "$STR_ASCZ_CowboyHat_Black";
-	picture = "\ascz_headgear\data\UI\icon_CowboyHat_black.paa";
-	
-	    mass = 6;
-		passThrough = 1;
-		hiddenSelectionsTextures[] = {"\ascz_headgear\data\CowboyHat_black_co.paa"};
-	    class ItemInfo: ItemInfo{};
-	};
-	class H_Cowboy_Hat_green: H_Cowboy_Hat_black
-	{
-	author = "ASCZ";
-	displayName = "$STR_ASCZ_CowboyHat_Green";
-	picture = "\ascz_headgear\data\UI\icon_CowboyHat_green.paa";
-	
-	    mass = 6;
-		passThrough = 1;
-		hiddenSelectionsTextures[] = {"\ascz_headgear\data\CowboyHat_green_co.paa"};
-	    class ItemInfo: ItemInfo{};
-	};
-	class H_Cowboy_Hat_white: H_Cowboy_Hat_green
-	{
-	author = "ASCZ";
-	displayName = "$STR_ASCZ_CowboyHat_White";
-	picture = "\ascz_headgear\data\UI\icon_CowboyHat_white.paa";
-	
-	    mass = 6;
-		passThrough = 1;
-		hiddenSelectionsTextures[] = {"\ascz_headgear\data\CowboyHat_white_co.paa"};
-	    class ItemInfo: ItemInfo{};
-        };
-    };
