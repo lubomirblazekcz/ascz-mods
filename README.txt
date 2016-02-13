38bd00d70357f34d012e0bd507661399b521b6d7
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2016-02-13 21:43:27 +0100

    @

--- README.txt
+++
@@ -1,3 +0,0 @@
-There should be readme, but since I'm lazy, there is only some random text, lol.
-
-Anyway, in this repo, there should be some Arma related stuff.
--- ascz_cup_vehicles_compability/mv22.hpp
+++
@@ -1,8 +0,0 @@
-
-	class CUP_B_MV22_USMC: Plane_Base_F
-	{
-		crew = "rhsusf_usmc_marpat_wd_helipilot";
-		faction = "rhs_faction_usmc_wd";
-		typicalCargo[] = {"rhsusf_usmc_marpat_wd_helipilot"};
-		vehicleClass = "rhs_vehclass_aircraft";
-	};
